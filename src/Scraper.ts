c526d4a562e6522932fdd9bd8ad5f1c505f18f97
Bostjan Janzovnik <bostjan.janzovnik@inova.si>
2021-03-10 21:26:40 +0100

    Add data extractor methods

--- src/Scraper.ts
+++ src/Scraper.ts
@@ -1,5 +1,5 @@
 import moment from "moment"
-import puppeteer, { Page } from "puppeteer"
+import puppeteer, { ElementHandle, Page } from "puppeteer"
 import { Config } from "./config"
 import { Ad } from "./interfaces"
 
@@ -38,53 +38,25 @@ export class Scraper {
     }
 
     private async getPageAds(page: Page): Promise<Ad[]> {
-        const adListItems = await page.$$(".EntityList--ListItemRegularAd>ul.EntityList-items>li.EntityList-item--Regular")
+        const adListItems = await page.$$("ul.EntityList-items>li.EntityList-item")
 
         return await adListItems.reduce(async (acc, adListItem) => {
             const accumulator = await acc
 
             try {
-                // id
-                const adId = await adListItem.$eval("h3.entity-title>a", el => el.getAttribute("name"))
-
-                // title
-                const adTitle = await adListItem.$eval("h3.entity-title>a", el => el.innerHTML)
-
-                // url
-                const urlPath = await adListItem.$eval("h3.entity-title>a", el => el.getAttribute("href"))
-                const adUrl = `${this.config.bolhaUrl}${urlPath}`
-
-                // published date
-                const adPublishedDateAttr = await adListItem.$eval("div.entity-pub-date>.date", el => el.getAttribute("datetime"))
-                const adPublishedDate = adPublishedDateAttr ? moment(adPublishedDateAttr.toString()).format("DD.MM.YYYY HH:mm") : undefined
-
-                // price
-                const adPriceHtml = await adListItem.$eval("div.entity-prices .price", el => el.innerHTML)
-                const adPriceMatch = adPriceHtml ? adPriceHtml.toString().match(/[0-9.,]+/) : undefined
-                const adPrice = adPriceMatch?.length ? adPriceMatch[0] : undefined
-
-                // currency
-                const adPriceCurrency = await adListItem.$eval("div.entity-prices .currency", el => el.innerHTML)
-
-                // description
-                const adDescriptionHtml = await adListItem.$eval("div.entity-description>.entity-description-main", el => el.innerHTML)
-                const adDescription = adDescriptionHtml
-                    ? adDescriptionHtml
-                          .toString()
-                          .trim()
-                          .replace(/<(.|\n)*?>/g, "")
-                          .replace(/\s\s+/g, " ")
-                    : undefined
+                const adId = await Scraper.getAdId(adListItem)
+                const adTitle = await Scraper.getAdTitle(adListItem)
+                const adUrl = await this.getAdUrl(adListItem)
 
                 if (adId && adTitle && adUrl) {
                     accumulator.push({
-                        id: adId.toString(),
-                        title: adTitle.toString(),
-                        url: adUrl.toString(),
-                        publishedDate: adPublishedDate,
-                        price: adPrice,
-                        currency: adPriceCurrency.toString(),
-                        description: adDescription,
+                        id: adId,
+                        title: adTitle,
+                        url: adUrl,
+                        publishedDate: await Scraper.getAdPublishedDate(adListItem),
+                        price: await Scraper.getAdPrice(adListItem),
+                        currency: await Scraper.getAdPriceCurrency(adListItem),
+                        description: await Scraper.getAdDescription(adListItem),
                     })
                 }
             } catch (e) {
@@ -93,5 +65,56 @@ export class Scraper {
 
             return accumulator
         }, Promise.resolve([] as Ad[]))
+
+        // TODO: iterate through results pages
+    }
+
+    private static async getAdId(adListItem: ElementHandle): Promise<string | undefined> {
+        const adId = await adListItem.$eval("h3.entity-title>a", el => el.getAttribute("name"))
+
+        return adId ? adId.toString() : undefined
+    }
+
+    private static async getAdTitle(adListItem: ElementHandle): Promise<string | undefined> {
+        const adTitle = await adListItem.$eval("h3.entity-title>a", el => el.innerHTML)
+
+        return adTitle ? adTitle.toString() : undefined
+    }
+
+    private async getAdUrl(adListItem: ElementHandle): Promise<string | undefined> {
+        const urlPath = await adListItem.$eval("h3.entity-title>a", el => el.getAttribute("href"))
+
+        return urlPath ? `${this.config.bolhaUrl}${urlPath.toString()}` : undefined
+    }
+
+    private static async getAdPublishedDate(adListItem: ElementHandle): Promise<string | undefined> {
+        const adPublishedDateAttr = await adListItem.$eval("div.entity-pub-date>.date", el => el.getAttribute("datetime"))
+
+        return adPublishedDateAttr ? moment(adPublishedDateAttr.toString()).format("DD.MM.YYYY HH:mm") : undefined
+    }
+
+    private static async getAdPrice(adListItem: ElementHandle): Promise<string | undefined> {
+        const adPriceHtml = await adListItem.$eval("div.entity-prices .price", el => el.innerHTML)
+        const adPriceMatch = adPriceHtml ? adPriceHtml.toString().match(/[0-9.,]+/) : undefined
+
+        return adPriceMatch?.length ? adPriceMatch[0] : undefined
+    }
+
+    private static async getAdPriceCurrency(adListItem: ElementHandle): Promise<string | undefined> {
+        const adPriceCurrency = await adListItem.$eval("div.entity-prices .currency", el => el.innerHTML)
+
+        return adPriceCurrency ? adPriceCurrency.toString() : undefined
+    }
+
+    private static async getAdDescription(adListItem: ElementHandle): Promise<string | undefined> {
+        const adDescriptionHtml = await adListItem.$eval("div.entity-description>.entity-description-main", el => el.innerHTML)
+
+        return adDescriptionHtml
+            ? adDescriptionHtml
+                  .toString()
+                  .trim()
+                  .replace(/<(.|\n)*?>/g, "")
+                  .replace(/\s\s+/g, ", ")
+            : undefined
     }
 }
